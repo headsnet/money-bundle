b0cb37322c8482d02cf957c9fa6db8bf92caf1b8
Ben Roberts <ben@headsnet.com>
2022-08-13 14:14:56 +0200

    Automatically register Currency DBAL type

--- src/HeadsnetMoneyBundle.php
+++ src/HeadsnetMoneyBundle.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Symfony HeadsnetMoneyBundle.
  *
- * (c) Headstrong Internet Services Ltd 2021
+ * (c) Headstrong Internet Services Ltd 2022
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,38 +11,55 @@
 namespace Headsnet\MoneyBundle;
 
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\Persistence\ManagerRegistry;
+use Headsnet\MoneyBundle\Doctrine\DBAL\Types\CurrencyType;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * Bundle configuration
- */
 class HeadsnetMoneyBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
-	/**
-	 * Here we add a compiler pass to add a Doctrine Mapping for the Money embeddable model
-	 *
-	 * @param ContainerBuilder $container
-	 */
-	public function build(ContainerBuilder $container)
-	{
-		parent::build($container);
+        $this->addDoctrineMapping($container);
+    }
 
-		$modelDir = realpath(__DIR__.'/Doctrine/Mapping');
-		$mappings = [
-			$modelDir => 'Money',
-		];
+    public function boot(): void
+    {
+        $this->addCurrencyColumnType();
+    }
 
-		if (class_exists(DoctrineOrmMappingsPass::class))
-		{
-			$container->addCompilerPass(
-				DoctrineOrmMappingsPass::createXmlMappingDriver(
-					$mappings,
-					['doctrine.orm.entity_manager'],
-					false
-				));
-		}
-	}
+    private function addDoctrineMapping(ContainerBuilder $container): void
+    {
+        $modelDir = realpath(__DIR__ . '/Doctrine/Mapping');
+        $mappings = [
+            $modelDir => 'Money',
+        ];
 
+        if (class_exists(DoctrineOrmMappingsPass::class)) {
+            $container->addCompilerPass(
+                DoctrineOrmMappingsPass::createXmlMappingDriver(
+                    $mappings,
+                    ['doctrine.orm.entity_manager'],
+                    false
+                )
+            );
+        }
+    }
+
+    private function addCurrencyColumnType(): void
+    {
+        if (!Type::hasType('currency')) {
+            Type::addType('currency', CurrencyType::class);
+        }
+
+        /** @var ManagerRegistry $registry */
+        $registry = $this->container->get('doctrine');
+
+        foreach ($registry->getConnections() as $connection) {
+            $connection->getDatabasePlatform()->registerDoctrineTypeMapping('currency', 'currency');
+        }
+    }
 }
